db926295c678d35275d8c64e4b4dc5703a7e1e6f
Gianfranco Manrique Valentín <Gianfranco Manrique Valentn>
2017-02-04 00:05:17 -0500

    Primeras rutas

--- app.js
+++ app.js
@@ -3,12 +3,81 @@ bodyparser=require('body-parser'),
 mongoose=require('mongoose'),
 app=express();
 
+app.use(bodyparser.urlencoded({extended:true}));
+
+mongoose.connect('mongodb://localhost/sie');//nombre de BD
+
+var emprendimientoSchema= new mongoose.Schema({
+	nombre:String,
+	slogan:String,
+	logo:String,
+	categorias_asociadas: []
+		/*
+	categorias: {
+			type:String,
+			default:'compra y venta'
+	}
+	*/
+	});
+
+var Emprendimiento = mongoose.model('Emprendimiento', emprendimientoSchema)
+
+/*
+Emprendimiento.create({
+	nombre:'ASEP',
+	slogan:'De la idea del negocio.',
+	logo:'https://asep.pe/wp-content/uploads/2016/08/logo_asep_nw.png',
+	categorias_asociadas:[
+		{nombre_categoria:'compra y venta'}
+	]
+
+});
+*/
+
 app.set('view engine','ejs');
 
 app.get('/',function(req,res){
-	res.render('landing');
+
+	Emprendimiento.find({}, function (err, emprendimientos) {
+		if (err) {
+			console.log(err)
+		}
+		else {			
+		res.render('landing', {emprendimientos: emprendimientos})
+		}
+	})
 });
+
+app.post('/',function(req,res){
+	
+	console.log('Desde post');
+	var nombre=req.body.nombre;
+	var logo=req.body.logo;
+	var emprendimiento={nombre:nombre,logo:logo};
+	console.log(emprendimiento);
+	Emprendimiento.create(emprendimiento, function (err, emprendimientos) {
+		if (err) {
+			console.log(err)
+		}
+		else {	
+			console.log('desde creado');				
+		}
+	})
+
+	res.redirect('/');
+	
+	//res.send('PUT request to homepage');
+	
+});
+
+app.get('/new',function(req,res){
+
+	res.render('new');
+	
+});
+
 
 app.listen(3000,function(){
 	console.log('Server corriendo.');
 });
+
